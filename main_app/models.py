1b37c0fdf14f274eea056fb3391398ccf80e3d93
DeMarcus Brown <demarcus.m.brown@gmail.com>
2022-10-21 12:17:23 -0400

    database displays on site

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    years = models.IntegerField()
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Employee
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def employees_index(request):
+    employees = Employee.objects.all()
+    return render(request, 'employees/index.html',{'employees': employees} )
